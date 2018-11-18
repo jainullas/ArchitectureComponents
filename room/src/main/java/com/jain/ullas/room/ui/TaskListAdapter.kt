01c84cf3d59fd154c66a0b155abf253f964c48a0
ullas <ullasjain21@gmail.com>
2018-11-18 17:16:27 +0530

    Added UI elements

---
+++ room/src/main/java/com/jain/ullas/room/ui/TaskListAdapter.kt
@@ -0,0 +1,39 @@
+package com.jain.ullas.room.ui
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jain.ullas.room.R
+import com.jain.ullas.room.database.Task
+import com.jain.ullas.room.ui.TaskListAdapter.TaskViewHolder
+import kotlinx.android.synthetic.main.item_task_row.view.*
+
+class TaskListAdapter : RecyclerView.Adapter<TaskViewHolder>() {
+
+    private val tasks : MutableList<Task>  = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int) =
+        TaskViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_task_row, parent, false))
+
+    override fun getItemCount(): Int = tasks.size
+
+    override fun onBindViewHolder(viewHolder: TaskViewHolder, position: Int) {
+        viewHolder.bind(tasks[position])
+    }
+
+    fun addTask(task: Task){
+        if(!tasks.contains(task)){
+            tasks.add(task)
+            notifyItemInserted(tasks.size)
+        }
+    }
+
+
+    class TaskViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(task: Task){
+            itemView.taskTitle.text = task.title
+        }
+    }
+
+}
--- room/src/main/java/com/jain/ullas/room/ui/TasksActivity.kt
+++ room/src/main/java/com/jain/ullas/room/ui/TasksActivity.kt
@@ -1,4 +1,75 @@
 package com.jain.ullas.room.ui
 
-class TasksActivity {
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import com.jain.ullas.room.R
+import com.jain.ullas.room.database.AppDatabase
+import com.jain.ullas.room.database.Task
+import com.jain.ullas.room.database.TaskDao
+import kotlinx.android.synthetic.main.activity_tasks.*
+import kotlinx.android.synthetic.main.content_tasks.*
+import kotlin.concurrent.thread
+
+class TasksActivity : AppCompatActivity() {
+
+    private lateinit var database : AppDatabase
+    private lateinit var taskDao : TaskDao
+    private lateinit var taskListAdapter: TaskListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasks)
+        setSupportActionBar(toolbar)
+
+        database = AppDatabase.getInstance(this)
+        taskDao = database.dao()
+        taskListAdapter = TaskListAdapter()
+        tasksList.apply {
+            adapter = taskListAdapter
+            layoutManager = LinearLayoutManager(this@TasksActivity)
+        }
+        refreshAllTasks()
+        fab.setOnClickListener {
+            addTask()
+        }
+
+
+    }
+
+    private fun refreshAllTasks() {
+        thread {
+            val allTasks = taskDao.getAll()
+            runOnUiThread {
+                allTasks.forEach { taskListAdapter.addTask(it) }
+            }
+
+        }
+    }
+
+    private fun addTask() {
+        val title = taskTitleInput.text.toString()
+        if(title.isBlank()){
+            Snackbar.make(toolbar, "Task title is required", Snackbar.LENGTH_SHORT).show()
+            return
+        }
+        thread {
+            taskDao.insert(Task(title = title))
+        }
+        taskTitleInput.text.clear()
+    }
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.menu_overflow_delete, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when(item.itemId){
+//            R.id.
+//        }
+//    }
+
+
 }
